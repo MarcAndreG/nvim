63e5b28a73e42649e5ac55b93cb0ac074471a559
Marc-Andre G. <ma@godin.co>
2024-05-01 14:17:48 -0400

    Adding view for telescope for modified files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fg', builtin.git_status, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
       vim.keymap.set("n", "<leader>bp", ":bp<CR>", {})
       vim.keymap.set("n", "<leader>bn", ":bn<CR>", {})
